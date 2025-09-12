b82ba32bd4528f4f0092c409c80b61b7531d0b4e
yatheeshv <yatheeshvv@gmail.com>
2025-09-12 16:40:07 +0530

    'sdfv'

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import api from './api/api';
+import api from '../api/api';
 
 function Login() {
   const [email, setEmail] = useState('');
